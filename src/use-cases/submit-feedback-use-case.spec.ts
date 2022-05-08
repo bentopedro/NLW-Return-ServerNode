600f5d01a4c202a473afe67787bb022a0aa49447
bentopedro <63908808+bentopedro@users.noreply.github.com>
2022-05-08 20:56:27 +0100

    4th Test With More it should be
    Analysed coverage 100%

--- src/use-cases/submit-feedback-use-case.spec.ts
+++ src/use-cases/submit-feedback-use-case.spec.ts
@@ -1,16 +1,40 @@
 import { SubmitFeedbackUseCase } from "./submit-feedback-use-case"
 
+const feedback = new SubmitFeedbackUseCase(
+    { create: async () => {}},
+    { sendMail: async () => {}}
+)
+
 describe('Submit feedback', () => {
     it('should be able to submit a feedback', async () => {
-        const feedback = new SubmitFeedbackUseCase(
-            { create: async () => {}},
-            { sendMail: async () => {}}
-        )
+        await expect(feedback.execute({
+            type: 'BUG',
+            comment: 'A new comment',
+            screenshot: 'data:image/png;base64,ksdansklnjkfgx'
+        })).resolves.not.toThrow();
+    });
 
+    it('should not be able to submit a feedback without type', async () => {        
+        await expect(feedback.execute({
+            type: '',
+            comment: 'A new comment',
+            screenshot: 'data:image/png;base64,ksdansklnjkfgx'
+        })).rejects.toThrow();
+    });
+
+    it('should not be able to submit a feedback without comment', async () => {        
+        await expect(feedback.execute({
+            type: 'BUG',
+            comment: '',
+            screenshot: 'data:image/png;base64,ksdansklnjkfgx'
+        })).rejects.toThrow();
+    });
+
+    it('should not be able to submit a feedback with invalid screenshot format', async () => {        
         await expect(feedback.execute({
             type: 'BUG',
             comment: 'A new comment',
             screenshot: 'photo.png'
-        })).resolves.not.toThrow();
-    })
+        })).rejects.toThrow();
+    });
 })
